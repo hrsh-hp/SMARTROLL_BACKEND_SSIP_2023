a20f71e90cd06d24d757983506bd3ee1d1be4e21
Manav1011 <manavshah1011.ms@gmail.com>
2023-11-27 23:31:49 +0530

    Set a static route

--- SMARTROLL/urls.py
+++ SMARTROLL/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path,include,re_path
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 from rest_framework import permissions
@@ -23,6 +23,7 @@ from .views import check_server_avaibility,check_token_authenticity,student_regi
 from django.views.generic import TemplateView
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -50,4 +51,6 @@ urlpatterns = [
     path('session/',include('Session.urls')),
     path('student/',include('Student.urls')),    
     path('api_endpoints/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-]+ static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+    re_path(r"^media/(?P<path>.*)$", serve, {"document_root": settings.MEDIA_ROOT}),
+    re_path(r"^static/(?P<path>.*)$", serve, {"document_root": settings.STATIC_ROOT}),
+]
