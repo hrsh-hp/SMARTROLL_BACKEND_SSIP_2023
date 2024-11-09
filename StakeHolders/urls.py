8ac24759543e519fa00548887f9949eff6153592
Manav1011 <manavshah1011.ms@gmail.com>
2024-11-09 18:21:20 +0530

    urls.py

--- StakeHolders/urls.py
+++ StakeHolders/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import CustomTokenObtainPairView,CustomTokenRefreshView,check_token_authenticity,student_register,forgot_password
+from .views import CustomTokenObtainPairView,CustomTokenRefreshView,check_token_authenticity,student_register,forgot_password,set_new_password_for_student
 
 urlpatterns = [
+    path('api/set_new_password_for_student/', set_new_password_for_student, name='set_new_password_for_student'),
     path('api/login/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', CustomTokenRefreshView.as_view(), name='token_refresh'),    
     path('api/check_token_authenticity', check_token_authenticity, name='check_token_authenticity'),
